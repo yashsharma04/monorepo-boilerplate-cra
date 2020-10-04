33e36e66eb27ad1969efc158dd387b45040909af
Yash Sharma <95yashsharma@gmail.com>
2020-10-04 12:33:15 +0530

    Monorepo setup initial commit

---
+++ packages/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <button>Submit here</button>
+    )
+}
+
+export default Button;
---
+++ packages/sub-app-1/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@monorepo/components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button />
+      </header>
+    </div>
+  );
+}
+
+export default App;
